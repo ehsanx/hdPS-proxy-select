869a7bee508831fe62fa629452f9a8ec1a2245e5
leiyanglcr <leiyanglcr@gmail.com>
2024-08-15 12:08:45 -0700

    load the numProxy & RD & SE table of simulation results

---
+++ simResults_scenario/load_simResults.R
@@ -0,0 +1,14 @@
+# 1_bross
+load("1_bross/bross.RData")
+
+# 2_hybrid
+load("2_hybrid/hybrid.RData")
+
+# 3_lasso
+load("3_lasso/lasso.RData")
+
+# 4_enet
+load("4_enet/enet.RData")
+
+# 7_xgboost
+load("7_xgboost/xgboost.RData")
